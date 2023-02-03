c173e8f28bad1bd5effd7dce14024121d6ff7dc9
JerryBian <JerryBian@outlook.com>
2023-02-03 13:24:21 +0800

    disable CGO

--- script/build-binary.sh
+++ script/build-binary.sh
@@ -9,7 +9,7 @@ echo version is $VER
 echo commit hash is $GITHASH
 echo Build time is $BUILDTIME
 
-env GOOS=darwin GOARCH=amd64 go build -v -ldflags "-X 'main.AppVer=$VER' -X 'main.BuildTime=$BUILDTIME' -X 'main.GitHash=$GITHASH'" -o _/darwin-amd64/wheel *.go 
-env GOOS=linux GOARCH=amd64 go build -v -ldflags "-X 'main.AppVer=$VER' -X 'main.BuildTime=$BUILDTIME' -X 'main.GitHash=$GITHASH'" -o _/linux-amd64/wheel *.go
-env GOOS=linux GOARCH=arm64 go build -v -ldflags "-X 'main.AppVer=$VER' -X 'main.BuildTime=$BUILDTIME' -X 'main.GitHash=$GITHASH'" -o _/linux-arm64/wheel *.go
-env GOOS=windows GOARCH=amd64 go build -v -ldflags "-X 'main.AppVer=$VER' -X 'main.BuildTime=$BUILDTIME' -X 'main.GitHash=$GITHASH'" -o _/windows-amd64/wheel.exe *.go
+env GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -v -ldflags "-X 'main.AppVer=$VER' -X 'main.BuildTime=$BUILDTIME' -X 'main.GitHash=$GITHASH'" -o _/darwin-amd64/wheel *.go 
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -v -ldflags "-X 'main.AppVer=$VER' -X 'main.BuildTime=$BUILDTIME' -X 'main.GitHash=$GITHASH'" -o _/linux-amd64/wheel *.go
+env GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -v -ldflags "-X 'main.AppVer=$VER' -X 'main.BuildTime=$BUILDTIME' -X 'main.GitHash=$GITHASH'" -o _/linux-arm64/wheel *.go
+env GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -v -ldflags "-X 'main.AppVer=$VER' -X 'main.BuildTime=$BUILDTIME' -X 'main.GitHash=$GITHASH'" -o _/windows-amd64/wheel.exe *.go
